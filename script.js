4f3187e44522e8e25e520869f406ae8280ab8755
RobDab <roberto.dabb@gmail.com>
2022-09-28 19:02:09 +0200

    corrected approach

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-let slides = document.getElementsByClassName('slide')
-console.log(slides)
+let slideShow = document.getElementsByClassName('slide')
+console.log(slideShow)
+let i=0
 
-// for(let i=0;i<slides.length; i++){
-//     slides[i].style.display = "none";
-// }
+console.log(slideShow[i])
+slideShow[i].classList.add('showSlide')
+// add showSlide class to the first slide in order to give its style and show it with display=block.
+function next(){
+    slideShow[i].classList.remove('showSlide')
+    // hide the current slide before going on and showing the next one.
+
+    i++
+    if(i===slideShow.length){
+        i=0
+    }
+    // IF is necessary to avoi reading and undefined element when i gets larger than array.length.
+
+    slideShow[i].classList.add('showSlide')
+}
+
+function previous(){
+    slideShow[i].classList.remove('showSlide')
+    i--
+    if(i<0){
+        i=slideShow.length-1
+    }
+    slideShow[i].classList.add('showSlide')
+}
 
